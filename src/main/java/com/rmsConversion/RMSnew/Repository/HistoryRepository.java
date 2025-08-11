9c5dc90fbe8dc375f4418a1553b01193780ee726
ravi <ravisakhavala29@gmail.com>
2025-08-11 16:46:12 +0530

    Feat: add nacessary model and create gap analysis report

--- src/main/java/com/rmsConversion/RMSnew/Repository/HistoryRepository.java
+++ src/main/java/com/rmsConversion/RMSnew/Repository/HistoryRepository.java
@@ -1,9 +1,19 @@
 package com.rmsConversion.RMSnew.Repository;
 
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.rmsConversion.RMSnew.Model.History;
 
 @Repository
 public interface HistoryRepository extends JpaRepository<History, Long> {
+    @Query(value = "SELECT device_id,device_date, system_date FROM history " +
+            "WHERE device_id = :deviceId AND device_date BETWEEN :startDate AND :endDate " +
+            "ORDER BY device_date ASC", nativeQuery = true)
+    List<Object[]> findDeviceDataInRange(@Param("deviceId") Long deviceId,
+                                  @Param("startDate") Date startDate,
+                                  @Param("endDate") Date endDate);
 }
